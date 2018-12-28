fbe0d6f8aa0f1f5c464c4acbfa5a81054d544ef2
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2018-12-28 06:45:05 +0100

    Handled forgot-password and login pages

--- forgot-password.php
+++ forgot-password.php
@@ -7,9 +7,9 @@
 	<div class="login-wrap customscroll d-flex align-items-center flex-wrap justify-content-center pd-20">
 		<div class="login-box bg-white box-shadow pd-30 border-radius-5">
 			<img src="vendors/images/login-img.png" alt="login" class="login-img">
-			<h2 class="text-center mb-30">Forgot Password</h2>
+			<h2 class="text-center mb-30">Mot de passe oublié</h2>
 			<form>
-				<p>Enter your email address to reset your password</p>
+				<p>Entrez votre adresse mail pour réinitialiser votre mot de passe</p>
 				<div class="input-group custom input-group-lg">
 					<input type="text" class="form-control" placeholder="Email">
 					<div class="input-group-append custom">
@@ -23,11 +23,11 @@
 								use code for form submit
 								<input class="btn btn-primary btn-lg btn-block" type="submit" value="Submit">
 							-->
-							<a class="btn btn-primary btn-lg btn-block" href="index.php">Submit</a>
+							<a class="btn btn-primary btn-lg btn-block" href="index.php">Envoyer</a>
 						</div>
 					</div>
 					<div class="col-sm-6">
-						<div class="forgot-password"><a href="login.php" class="btn btn-outline-primary btn-lg btn-block">Sign In</a></div>
+						<div class="forgot-password"><a href="login.php" class="btn btn-outline-primary btn-lg btn-block">Se connecter</a></div>
 					</div>
 				</div>
 			</form>
--- login.php
+++ login.php
@@ -7,10 +7,10 @@
 	<div class="login-wrap customscroll d-flex align-items-center flex-wrap justify-content-center pd-20">
 		<div class="login-box bg-white box-shadow pd-30 border-radius-5">
 			<img src="vendors/images/login-img.png" alt="login" class="login-img">
-			<h2 class="text-center mb-30">Login</h2>
+			<h2 class="text-center mb-30">Connexion</h2>
 			<form>
 				<div class="input-group custom input-group-lg">
-					<input type="text" class="form-control" placeholder="Username">
+					<input type="text" class="form-control" placeholder="Nom d'utilisateur">
 					<div class="input-group-append custom">
 						<span class="input-group-text"><i class="fa fa-user" aria-hidden="true"></i></span>
 					</div>
@@ -25,14 +25,14 @@
 					<div class="col-sm-6">
 						<div class="input-group">
 							<!--
-								use code for form submit
+								Inserer le code pour la connexion de l'utilisateur
 								<input class="btn btn-outline-primary btn-lg btn-block" type="submit" value="Sign In">
 							-->
-							<a class="btn btn-outline-primary btn-lg btn-block" href="index.php">Sign In</a>
+							<a class="btn btn-outline-primary btn-lg btn-block" href="index.php">Me connecter</a>
 						</div>
 					</div>
 					<div class="col-sm-6">
-						<div class="forgot-password padding-top-10"><a href="forgot-password.php">Forgot Password</a></div>
+						<div class="forgot-password padding-top-10"><a href="forgot-password.php">Mot de passe oublié</a></div>
 					</div>
 				</div>
 			</form>
